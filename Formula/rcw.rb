fc6b52bd25fabb31c253dbeab0caccd3ce2b50c2
Lyubomir Kyuchukov <45465568+lkyuchukov@users.noreply.github.com>
2022-10-30 23:22:12 +0200

    Add 0.3.1 release

--- Formula/rcw.rb
+++ Formula/rcw.rb
@@ -1,9 +1,9 @@
 class Rcw < Formula
     desc "wc in Rust"
     homepage "https://github.com/lkyuchukov/rcw"
-    url "https://github.com/lkyuchukov/rcw/releases/download/v0.3.0/rcw.tar.gz"
-    sha256 "889e1deba94f22abcf77e3251885b1f8bbfa5c868398760030ace2139970abee"
-    version "0.3.0"
+    url "https://github.com/lkyuchukov/rcw/releases/download/v0.3.1/rcw.tar.gz"
+    sha256 "37df558822d29ea6ef627e4dc1d4bce50f9102c9470b26f16f4da64ceb75a96e"
+    version "0.3.1"
   
     def install
       bin.install "rcw"
